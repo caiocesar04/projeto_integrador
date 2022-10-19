25495f1af1e18ea5d5e56f0b8f5ef440711bba2c
caiocesar04 <55161837+caiocesar04@users.noreply.github.com>
2022-10-19 11:03:48 -0300

    Add files via upload

---
+++ brashop/app/controllers/AnuncioController.php
@@ -0,0 +1,43 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once __DIR__ . "/../repository/AnuncioRepository.php";
+//require_once __DIR__ . "/Controller.php";
+
+$controlleranuncio = new ControllerAnuncio();
+
+class ControllerAnuncio{
+
+	function __construct(){
+		if(isset($_POST["action"])){
+			$action = $_POST["action"];
+		}else if(isset($_GET["action"])){
+			$action = $_GET["action"];
+		}
+		//print_r($action);
+
+		if(isset($action)){
+			$this->callAction($action);
+		}else{
+			$msg = "Nenhuma acao a ser processada...";
+            print_r($msg);
+			//include_once "index.php";
+		}
+	}
+
+    public function callAction(string $functionName = null){
+        //print_r($functionName);
+        if (method_exists($this, $functionName)) {
+            $this->$functionName();
+        
+        } else if(method_exists($this, "preventDefault")) {
+            $met = "preventDefault";
+            $this->$met();
+        
+        } else {
+            throw new BadFunctionCallException("Usecase not exists");
+        }
+    }
