8d5645c46821a86d3788e6da21bfbe1a72da251d
caiocesar04 <55161837+caiocesar04@users.noreply.github.com>
2022-10-19 15:51:56 -0300

    Update marketplace.sql

--- brashop/marketplace.sql
+++ brashop/marketplace.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 14-Set-2022 às 02:37
--- Versão do servidor: 10.4.24-MariaDB
--- versão do PHP: 8.1.6
+-- Host: localhost:3306
+-- Tempo de geração: 19-Out-2022 às 15:50
+-- Versão do servidor: 8.0.30-0ubuntu0.20.04.2
+-- versão do PHP: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -24,16 +25,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `anuncios`
+--
+
+CREATE TABLE `anuncios` (
+  `id` int NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `preco` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `anuncios`
+--
+
+INSERT INTO `anuncios` (`id`, `nome`, `preco`) VALUES
+(5, 'ps4', 1000);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `usuarios`
 --
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `nome` varchar(45) NOT NULL,
   `email` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `data_nasc` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `usuarios`
@@ -41,11 +61,20 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `data_nasc`) VALUES
 (4, 'Caio', 'caio@gmail.com', 'abc123', '2004-04-20'),
-(11, 'Qualquer', 'testando@gmail.com', 'teste', '2000-10-09');
+(11, 'Qualquer', 'testando@gmail.com', 'teste', '2000-10-09'),
+(13, 'gagqtqwetqw', 'abc@email.com', '123445', '1999-11-11'),
+(14, 'gagqtqwetqw', 'aaaaa@email.com', '123445', '1999-11-11'),
+(15, 'teste', 'aaa@emial.com', '12345', '2000-11-11');
 
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices para tabela `anuncios`
+--
+ALTER TABLE `anuncios`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices para tabela `usuarios`
@@ -58,10 +87,16 @@ ALTER TABLE `usuarios`
 --
 
 --
+-- AUTO_INCREMENT de tabela `anuncios`
+--
+ALTER TABLE `anuncios`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
