e834cc43d6c5b54d127971de6ee33f9b8863608f
pedrohenriqueifpr <55161713+pedrohenriqueifpr@users.noreply.github.com>
2022-10-20 15:15:56 -0300

    Update marketplace.sql

--- brashop/marketplace.sql
+++ brashop/marketplace.sql
@@ -1,53 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.9.5deb2
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Tempo de geração: 20-Out-2022 às 14:56
--- Versão do servidor: 8.0.30-0ubuntu0.20.04.2
--- versão do PHP: 7.4.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `marketplace`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `anuncios`
---
-
 CREATE TABLE `anuncios` (
   `id` int NOT NULL,
   `nome` varchar(100) NOT NULL,
   `preco` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Extraindo dados da tabela `anuncios`
---
+);
 
 INSERT INTO `anuncios` (`id`, `nome`, `preco`) VALUES
 (11, 'ps4', 1000),
 (12, 'dfhrthrth', 23),
 (13, 'dfhrthrth', 23);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuarios`
---
 
 CREATE TABLE `usuarios` (
   `id` int NOT NULL,
@@ -55,11 +15,7 @@ CREATE TABLE `usuarios` (
   `email` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `data_nasc` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Extraindo dados da tabela `usuarios`
---
+);
 
 INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `data_nasc`) VALUES
 (4, 'Caio', 'caio@gmail.com', 'abc123', '2004-04-20'),
@@ -67,41 +23,13 @@ INSERT INTO `usuarios` (`id`, `nome`, `email`, `senha`, `data_nasc`) VALUES
 (17, 'ssss', 'email@email.com', '123', '3333-02-23'),
 (31, '456456', 'ca435345io@gmail.com', '3245234', '4535-03-05'),
 (34, '234234', 'caiougu8yygu@gmail.com', 'ouhgugkc', '0234-04-23');
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `anuncios`
---
 ALTER TABLE `anuncios`
   ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `usuarios`
---
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `anuncios`
---
 ALTER TABLE `anuncios`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT de tabela `usuarios`
---
 ALTER TABLE `usuarios`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
