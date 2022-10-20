ae076c757126930ed7add89ebf6fdefa4e74ed3f
pedrohenriqueifpr <55161713+pedrohenriqueifpr@users.noreply.github.com>
2022-10-20 14:47:18 -0300

    Update home.php

--- brashop/app/views/usuarios/home.php
+++ brashop/app/views/usuarios/home.php
@@ -14,7 +14,7 @@
   <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-  <a class="navbar-brand active" href="index.php">Brashop</a>
+  <a class="navbar-brand active" href="./UsuarioController.php?action=loadHome">Brashop</a>
   <button class="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="dropdown-toggle" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
