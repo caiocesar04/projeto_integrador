6d5296985119a6acd3b455ce3b38f07e24d49719
Pedro Henrique Alves dos Santos <pedro.henriqueifpr@gmail.com>
2022-11-16 14:29:23 -0300

    Update formLogin.php

--- brashop/app/views/usuarios/formLogin.php
+++ brashop/app/views/usuarios/formLogin.php
@@ -9,11 +9,9 @@
   </head>
   <body  >
   <?php
-	include_once __DIR__ . "/../helpers/mensagem.php";
-	//$caminho = __DIR__ . "/../helpers/mensagem.php";  
-	//print_r($caminho); 
-
+	include_once __DIR__ . "/../helpers/mensagem.php"; 
 ?>
+	  
 <h2>Login de usuarios</h2>
 
 <form action="./UsuarioController.php?action=login" method="POST">
