a512f822b32d44be928e9fc89502330e0ed09476
Pedro Henrique Alves dos Santos <pedro.henriqueifpr@gmail.com>
2022-11-16 15:06:11 -0300

    Update formLogin.php

--- brashop/app/views/usuarios/formLogin.php
+++ brashop/app/views/usuarios/formLogin.php
@@ -1,33 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" type="text/css" href="../views/helpers/css/bootstrap.min.css">
-    <script src="../views/helpers/js" type="text/javascript"></script>
-    <title>Brashop</title>
-  </head>
-  <body  >
-  <?php
-	include_once __DIR__ . "/../helpers/mensagem.php";
-	//$caminho = __DIR__ . "/../helpers/mensagem.php";  
-	//print_r($caminho); 
-
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport"content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../views/helpers/style.css">
+        <title> Brashop</title>
+    </head>
+    <body>
+    <?php
+	include_once __DIR__ . "/../helpers/mensagem.php"; 
 ?>
-<h2>Login de usuarios</h2>
-
-<form action="./UsuarioController.php?action=login" method="POST">
-<div class="mb-3">
-    <label>Email</label>
-    <input type="email" name="email"  class="form-control" required>
-<div class="mb-3">
-    <label>Senha</label>
-    <input type="password" name="senha" class="form-control" required>
-</div>
-<div class="mb-3">
-    <button type="submit" value="entrar" name="entrar" class="btn
-    btn-primary">Entrar</button>
-</form>		
-
-</body>
+        <div id="login">
+            <form class="card" action="./UsuarioController.php?action=login" method="POST">
+                <div class="card-header">
+                    <h2>Login</h2>
+                </div>
+                <div class="card-content">
+                    <div class="card-content-area">
+                    <label>Email</label>
+                    <input type="email" name="email"  class="form-control" required>
+                    </div>
+                    <div class="card-content-area">
+                    <label>Senha</label>
+                    <input type="password" name="senha" class="form-control" required>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <input type="submit" value="Login" class="submit">
+                    <a href="#" class="recuperar_senha">Esqueceu a senha?(not working)</a>
+                </div>
+            </form>
+        </div>
+    </body>
 </html>
