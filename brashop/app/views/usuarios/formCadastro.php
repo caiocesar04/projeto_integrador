bc45524e1fcf1df90142b2ee8a9d9b3c80eb74c7
Pedro Henrique Alves dos Santos <pedro.henriqueifpr@gmail.com>
2022-11-16 15:32:35 -0300

    Update formCadastro.php

--- brashop/app/views/usuarios/formCadastro.php
+++ brashop/app/views/usuarios/formCadastro.php
@@ -1,36 +1,37 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" type="text/css" href="../views/helpers/css/bootstrap.min.css">
-    <script src="../views/helpers/js" type="text/javascript"></script>
-    <title>Brashop</title>
-  </head>
-  <body>
-    
-<h1> Cadastre-se </h1>
-<form action="./UsuarioController.php?action=create" method="POST">
-    <input type="hidden" name="acao" value="cadastrar">
-<div class="mb-3">
-    <label>Nome:</label>
-    <input type="text" name="nome" class="form-control" required>
-</div>
-<div class="mb-3">
-    <label>Email:</label>
-    <input type="email" name="email" class="form-control" required>
-</div>
-<div class="mb-3">
-    <label>Senha:</label>
-    <input type="password" name="senha" class="form-control" required>
-</div>
-<div class="mb-3">
-    <label>Data de Nascimento:</label>
-    <input type="date" name="data_nasc" class="form-control" required>
-</div>
-<div class="mb-3">
-    <button type="submit" value="enviar" name="enviar" class="btn
-    btn-primary">Enviar</button>
-</div>
-</form>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport"content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../views/helpers/style.css">
+        <title> Brashop</title>
+    </head>
+    <body>
+    <?php
+	include_once __DIR__ . "/../helpers/mensagem.php"; 
+?>
+        <div id="title">
+            <form class="card" action="./UsuarioController.php?action=login" method="POST">
+                <div class="card-header">
+                    <h2>Cadastre-se</h2>
+                </div>
+                <div class="card-content">
+                    <div class="card-content-area">
+                    <label>Nome</label>
+                    <input type="text" name="nome"  class="form-control" required>
+                    <label>Email</label>
+                    <input type="email" name="email"  class="form-control" required>
+                    <label>Senha</label>
+                    <input type="password" name="senha" class="form-control" required>
+                    <label>Data de Nascimento:</label>
+                    <input type="date" name="data_nasc" class="form-control" required>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <input type="submit" value="Cadastre-se" class="submit">
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
