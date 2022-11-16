a30e4e3eb3b509c3671151bbfde5ebf5cedbb807
Pedro Henrique Alves dos Santos <pedro.henriqueifpr@gmail.com>
2022-11-16 15:57:12 -0300

    Update AnuncioController.php

--- brashop/app/controllers/AnuncioController.php
+++ brashop/app/controllers/AnuncioController.php
@@ -1,10 +1,6 @@
 <?php
-
-
 require_once __DIR__ . "/../repository/AnuncioRepository.php";
- 
 $controlleranuncio = new ControllerAnuncio();
-
 class ControllerAnuncio{
   
     function __construct(){
