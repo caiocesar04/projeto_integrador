77df1385383d1aa714b72304475598e2081aa98f
Pedro Henrique Alves dos Santos <pedro.henriqueifpr@gmail.com>
2022-12-14 16:57:11 -0300

    Update formCadastro.php

--- brashop/app/views/usuarios/formCadastro.php
+++ brashop/app/views/usuarios/formCadastro.php
@@ -1,38 +1,44 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport"content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="../views/helpers/css/bootstrap.min.css">
-        <link rel="stylesheet" href="../views/helpers/style.css">
-        <title> Brashop</title>
-    </head>
-    <body>
-    <?php
-	include_once __DIR__ . "/../helpers/mensagem.php"; 
-?>
-        <div id="title">
-            <form class="card col-md-10 mx-auto col-lg-5 p-4 p-md-5" action="./UsuarioController.php?action=create" method="POST">
-                <div class="card-header">
-                    <h2>Cadastre-se</h2>
-                </div>
-                <div class="card-content">
-                    <div class="card-content-area">
-                    <label>Nome</label>
-                    <input type="text" name="nome"  class="form-control" required>
-                    <label>Email</label>
-                    <input type="email" name="email"  class="form-control" required>
-                    <label>Senha</label>
-                    <input type="password" name="senha" class="form-control" required>
-                    <label>Data de Nascimento:</label>
-                    <input type="date" name="data_nasc" class="form-control" required>
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <input type="submit" value="Cadastre-se" class="submit">
-                </div>
-            </form>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=devide-widthm initial-scale=1.0">
+    <link rel="stylesheet" href="../views/helpers/styles.css">
+    <title>Brashop</title>
+</head>    
+<?php include_once __DIR__ . "/../helpers/mensagem.php";?>
+<body>
+<form action="./UsuarioController.php?action=create" method="POST">  
+<div class="main-login">
+    <div class="left-login">
+    <a href="./UsuarioController.php?action=loadHome" class="left-login-image">
+           <img src="../../imagens/brashop.png" class="left-login-image" alt="imagem">
+        </a>
+    </div>  
+    <div class="right-login">
+        <div class="card-login">
+            <h1>Cadastre-se</h1>
+            <div class="textfield">
+                <label for="usuario">Insira seu nome</label>
+                <input type="text" name="nome" class="form-control"  placeholder="Nome" required>
+            </div>
+            <div class="textfield">
+                <label for="usuario">Insira seu e-mail</label>
+                <input type="email" name="email" class="form-control" placeholder="E-mail" required>
+            </div>
+                <div class="textfield">
+                <label for="usuario">Insira sua senha</label>
+                <input type="password" name="senha" class="form-control" placeholder="Senha" required>
+            </div>
+            <div class="textfield">
+                <label>Data de Nascimento:</label>
+                <input type="date" name="data_nasc" class="form-control" required>
+             </div>
+            <button class="btn-login">Cadastre-se</button>
         </div>
-    </body>
+    </div>
+</div>
+</form>
+</body>
 </html>
