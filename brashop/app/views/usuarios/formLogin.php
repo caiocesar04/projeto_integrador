48fc2e6faace014e4a42fbc9556a3f7cd7a67736
Pedro Henrique Alves dos Santos <pedro.henriqueifpr@gmail.com>
2023-01-26 17:29:01 -0300

    Update formLogin.php

--- brashop/app/views/usuarios/formLogin.php
+++ brashop/app/views/usuarios/formLogin.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=devide-widthm initial-scale=1.0">
-    <link rel="stylesheet" href="../views/helpers/styles.css">
+    <link rel="stylesheet" href="../views/helpers/styless.css">
     <title>Brashop</title>
 </head>    
 <?php include_once __DIR__ . "/../helpers/mensagem.php";?>
